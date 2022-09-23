f342797537a27be92304fd490ccf8c561d93d90f
Darwin Luque <darwin.luque.98@gmail.com>
2022-09-23 12:11:46 -0600

    chore(mobile): init setup of redux

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { SafeAreaView, Text, StatusBar } from 'react-native';
+import { store } from './store';
 
 export const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Text>Chat App</Text>
       </SafeAreaView>
-    </>
+    </Provider>
   );
 };
 
