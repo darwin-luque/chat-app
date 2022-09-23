2696fbab755421242eb2463a1df47fabf1b687b0
Darwin Luque <darwin.luque.98@gmail.com>
2022-09-22 21:56:11 -0600

    chore(auth-api): remove basic template

--- apps/auth-api/src/main.ts
+++ apps/auth-api/src/main.ts
@@ -1,12 +1,8 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+import { AppModule } from './app.module';
 
-import { AppModule } from './app/app.module';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,9 +10,6 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
 }
 
 bootstrap();
