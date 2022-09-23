d18ca1db511743a1530ce477ae9ffd8db9e29d86
Darwin Luque <darwin.luque.98@gmail.com>
2022-09-23 00:04:06 -0600

    chore(nest0utils): add a pagination input dto and constants for default page values

--- libs/nest-utils/src/lib/dtos/pagination-input.dto.ts
+++ libs/nest-utils/src/lib/dtos/pagination-input.dto.ts
@@ -2,13 +2,13 @@ import { Transform } from 'class-transformer';
 import { IsNumber, IsOptional } from 'class-validator';
 
 export class PaginationInputDto {
-  @Transform(({ value }) => parseInt(value))
+  @Transform(({ value }) => value ? parseInt(value) : 0)
   @IsNumber()
   @IsOptional()
-  offset!: number;
+  offset?: number;
 
-  @Transform(({ value }) => parseInt(value))
+  @Transform(({ value }) => value ? parseInt(value) : 10)
   @IsNumber()
   @IsOptional()
-  limit!: number;
+  limit?: number;
 }
