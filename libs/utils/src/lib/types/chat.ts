19e78007b72a11f8ddfb66889c7f442eb4f84217
Darwin Luque <darwin.luque.98@gmail.com>
2022-09-26 15:22:05 -0600

    chore(utils): add shared typing for chat related stuff

---
+++ libs/utils/src/lib/types/chat.ts
@@ -0,0 +1,17 @@
+export interface IConversation {
+  id: string;
+  members: string[];
+  messages?: IMessage[];
+  lastMessage?: IMessage;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IMessage {
+  id: string;
+  conversation: IConversation
+  user: string;
+  body: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
--- libs/utils/src/lib/types/index.ts
+++ libs/utils/src/lib/types/index.ts
@@ -1,3 +1,4 @@
-export * from './media';
-export * from './auth';
 export * from './api';
+export * from './auth';
+export * from './chat';
+export * from './media';
